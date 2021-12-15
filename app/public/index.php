e41dca6456e8d13752b0680f2d7244e4f4f223de
louisehedman <louise.hedman@chasacademy.se>
2021-12-15 10:56:49 +0100

    add css file and template html

--- app/public/index.php
+++ app/public/index.php
@@ -1,11 +1,27 @@
-<?php
-$db = new PDO('mysql:host=mysql;dbname=todoapp', 'admin', 'secret');
+<?php include('php_db.php'); ?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" type="text/css" href="style.css">
+   <title>U04-to-do-app</title>
+</head>
+<body>
+   <h1>U04 - To do app</h1>
+   <form method="post" action="index.php" >
+   <div>
+      <button name="add" type="submit" value="add_task">Add task</button>
+</div>
+      <label>title</label>
+      <input type="text" name="title" value="">
+      <label>task</label>
+      <input type="text" name="task" value="">
 
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+   </form>
+</body>
+</html>
 
-$rows = $db->query('SELECT * FROM list ORDER BY title');
-foreach ($rows as $row) {
-   var_dump($row);
-}
 
-phpinfo();
+
---
+++ app/public/php_db.php
@@ -0,0 +1,11 @@
+<?php
+$db = new PDO('mysql:host=mysql;dbname=todoapp', 'admin', 'secret');
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$rows = $db->query('SELECT * FROM list ORDER BY title');
+foreach ($rows as $row) {
+   var_dump($row);
+}
+
+//phpinfo();
