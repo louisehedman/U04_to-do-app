aac66863292cc48ae2691985735deda50de830cd
louisehedman <louise.hedman@chasacademy.se>
2021-12-29 16:45:46 +0100

    update html css

--- app/public/index.php
+++ app/public/index.php
@@ -12,6 +12,9 @@ if (isset($_POST['add'])) {
    if (empty($_POST['title'])) {
       $notset = "Title can't be blank";
    }
+   else if (ctype_space($_POST['title']) || ctype_space($_POST['task'])){
+      $notset = "Input can't consist of whitespace";
+   }
     else{
       $query = <<<SQL
       INSERT INTO list (title, task, done) VALUES (:title, :task, 0);  
@@ -38,6 +41,12 @@ if (isset($_GET['update'])) {
 }
 
 if (isset($_POST['update'])) {
+   if (empty($_POST['title'])) {
+      $notset = "Title can't be blank";
+   }
+   else if (ctype_space($_POST['title']) || ctype_space($_POST['task'])){
+      $notset = "Input can't consist of whitespace";
+   }
    $id = $_POST['id'];
    $title = $_POST['title'];
    $task = $_POST['task'];
@@ -62,6 +71,7 @@ if (isset($_GET['delete'])) {
 if (isset($_GET['as'], $_GET['row'])) {
    $as = $_GET['as'];
    $row = $_GET['row'];
+   
    $query = $db->prepare("UPDATE list SET done = 1 WHERE id = :row");
    $query->execute(['row' => $row]);
 }
@@ -70,79 +80,81 @@ if (isset($_GET['as'], $_GET['row'])) {
 
 <!DOCTYPE html>
 <html lang="sv">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" type="text/css" href="styles.css">
-   <title>U04-to-do-app</title>
-</head>
-<body>
-   <header>
-   <h1>U04 - To do app</h1>
-   <img src="\assets\img\2022.jpg" />
-   </header>
-   <main>
-   <section>
-   <form method="post" action="index.php" >
-      <input type="hidden" name="id" value="<?php echo $id; ?>";>
-      <label>Title</label>
-      <input type="text" name="title" value="<?php echo $title; ?>">
-      <br>
-      <label>Task</label>
-      <input type="text" name="task" value="<?php echo $task; ?>">
-      <div>
-         <?php if ($update == true): ?>
-            <button type="submit" name="update" >update</button>
-         <?php else: ?>
-            <button type="submit" name="add" class="submitbtn">Add task</button>
-         <?php endif ?>
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <title>U04-to-do-app</title>
+   </head>
+   <body>
+      <div id="wrapper">
+         <header>
+            <h1>U04 - To do app</h1>
+            <img src="\assets\img\2022.jpg" />
+         </header>
+         <main>
+            <section>
+               <form method="post" action="index.php" >
+                  <input type="hidden" name="id" value="<?php echo $id; ?>";>
+                  <label>Title</label>
+                  <input type="text" name="title" value="<?php echo $title; ?>">
+                  <br>
+                  <label>Task&nbsp;</label>
+                  <input type="text" name="task" value="<?php echo $task; ?>">
+                  <div>
+                     <?php if ($update == true): ?>
+                        <button type="submit" name="update" class="submitbtn">update</button>
+                     <?php else: ?>
+                        <button type="submit" name="add" class="submitbtn">Add task</button>
+                     <?php endif ?>
+                  </div>
+                     <?php if (isset($notset)) { ?>
+                     <p class="error"><?php echo $notset; ?></p>
+                     <?php } ?>
+               </form>
+               
+               <?php $rows = $db->query('SELECT * FROM list'); ?>
+               
+               <table>
+               <thead>
+                  <tr>
+                     <th>ID</th>
+                     <th>Title</th>
+                     <th>Task</th>
+                     <th>Done</th>
+                     <th>Update</th>
+                     <th>Delete</th>
+                  </tr>
+               </thead>
+               <tbody>
+                     
+               <?php foreach ($rows as $row) { ?>
+                  <tr>
+                     <td> <?php echo $row['id']; ?> </td>
+                     <td class="row<?php echo $row['done'] ? ' done' : '' ?>"> <?php echo $row['title']; ?> </td>
+                     <td class="row<?php echo $row['done'] ? ' done' : '' ?>" > <?php echo $row['task']; ?> </td>
+                     <td>
+                        <?php if (!$row['done']) { ?>
+                           <a class="link" href="index.php?as=done&row=<?php echo $row['id']; ?>">&#9989;</a> 
+                        <?php } ?>
+                     </td>
+                     <td> 
+                        <?php if (!$row['done']) { ?>
+                           <a href="index.php?update=<?php echo $row['id']; ?>">&#9999;&#65039;</a> 
+                        <?php } ?> 
+                     </td>
+                     <td>
+                        <a class="link" href="index.php?delete=<?php echo $row['id']; ?>">&#x274C;</a> 
+                     </td>
+                  </tr>
+                  <?php } ?>
+               </tbody>
+            </section>
+         </main>
+         <footer>
+
+         </footer>
       </div>
-   </form>
-
-   <?php if (isset($notset)) { ?>
-	<p><?php echo $notset; ?></p>
-   <?php } ?>
-
-   <?php $rows = $db->query('SELECT * FROM list'); ?>
-
-   <table>
-	<thead>
-		<tr>
-			<th>ID</th>
-			<th>Title</th>
-         <th>Task</th>
-         <th>Done</th>
-         <th>Update</th>
-			<th>Delete</th>
-		</tr>
-	</thead>
-   <tbody>
-
-      <?php foreach ($rows as $row) { ?>
-         <tr>
-            <td> <?php echo $row['id']; ?> </td>
-				<td class="row<?php echo $row['done'] ? ' done' : '' ?>"> <?php echo $row['title']; ?> </td>
-            <td class="row<?php echo $row['done'] ? ' done' : '' ?>" > <?php echo $row['task']; ?> </td>
-            <td> 
-               <?php if (!$row['done']) { ?>
-               <a href="index.php?as=done&row=<?php echo $row['id']; ?>">&#9989;</a> 
-               <?php } ?>
-            </td>
-            <td> 
-               <?php if (!$row['done']) { ?>
-               <a href="index.php?update=<?php echo $row['id']; ?>">&#9999;&#65039;</a> 
-               <?php } ?> 
-            </td>
-            <td> <a href="index.php?delete=<?php echo $row['id']; ?>">&#x274C;</a> </td>
-			</tr>
-      <?php } ?>
-
-   </tbody>
-      </section>
-   </main>
-   <footer>
-
-   </footer>
-</body>
+   </body>
 </html>
